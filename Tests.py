60909ba1c2328b0e799387125acc5ea193b61187
Gualtiero <gualtyphone@me.com>
2017-12-17 06:10:30 +0000

    Ported to Mac for Christmas ;)

--- Tests.py
+++ Tests.py
@@ -11,7 +11,8 @@ pars = Pars()
 # load the file to game
 # game = hsDoc.from_xml_file(fileName)
 while input() != "quit":
-    f = open("C:\Program Files (x86)\Hearthstone\Logs\Power.log", "r")
+    f = open("/Applications/Hearthstone/Logs/Power.log", "r")
+    # f = open("C:\Program Files (x86)\Hearthstone\Logs\Power.log", "r")
     myList = []
     for line in f:
         myList.append(line)
