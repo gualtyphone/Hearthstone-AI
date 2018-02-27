e62a3f4b89f221edcaa5d4f8933a75669664c0c0
gualtyphone <gualtyphone@me.com>
2018-02-27 12:55:12 +0000

    More GUI Work

--- MainGUI.py
+++ MainGUI.py
@@ -3,6 +3,7 @@ import pyforms
 from pyforms import BaseWidget
 from pyforms.controls import *
 import Network
+import cv2
 
 network = Network.Network()
 
@@ -11,44 +12,57 @@ class HearthGUI(BaseWidget):
     def __init__(self):
         super(HearthGUI, self).__init__('Hearth-AI')
 
-        # Definition of the forms fields
-        self._runName = ControlText('Run name')
-        self._runButton = ControlButton('Run')
-        self._runOnceButton = ControlButton('Run Once')
-        self._epochDisplay = ControlButton('Epoch = 0')
-        self._boardStateTree = ControlTree('Boar State')
-        self._updateBoardState = ControlButton('Update Board State')
-        self._readyButton = ControlButton('Ready!')
-        self._modeSelect = ControlCombo('Data Loading Mode')
-
-        # Define the actions
-        self._runButton.value = self.__runNetwork
-        self._runOnceButton.value = self.__runNetworkOnce
-
-        self._modeSelect += ('Randomized Stored Data', 0)
-        self._modeSelect += ('Client Connect Windows', 1)
-        self._modeSelect += ('Client Connect MacOS', 2)
-
-        self._epochDisplay.enabled = False
-        self._readyButton.value = self.__ready
-        self._runName.value = network.RUN_NAME
-
+        # Define Main Menu
         self.mainmenu = [
             {'File': [
-                {'Run Once': self.__runNetworkOnce},
-                {'Run': self.__runNetwork},
+                {'Run Once': self._runNetworkOnce},
+                {'Run': self._runNetwork},
                 '-',
                 {'Exit': self._exit},
             ]
             }
         ]
 
+        # Definition of the forms fields
+        self._runName = ControlText('Run name')
+        self._readyButton = ControlButton('Press when ready!')
+
+        self._cardToPlayImage = ControlImage()
+        self._cardToPlayGL = ControlOpenGL()
+        self._optionToTakeDescription = ControlText('Play Description')
+
+        self._boardStateTree = ControlTree('Boar State')
+        self._updateBoardStateButton = ControlButton('Update Board State')
+
+        self._runButton = ControlButton('Run')
+        self._runOnceButton = ControlButton('Run Once')
+        self._epochDisplay = ControlButton('Epoch = 0')
+        self._modeSelect = ControlCombo('Data Loading Mode')
+
+        # Define the actions
+        self._runName.value = network.RUN_NAME
+        self._readyButton.value = self._ready
+
+        self._cardToPlayImage.value = cv2.imread('Cards/Alex.png', 1)
+        self._cardToPlayImage.repaint()
+
+        self._updateBoardStateButton.value = self._updateBoardState
+        self._runButton.value = self._runNetwork
+        self._runOnceButton.value = self._runNetworkOnce
+
+        self._modeSelect += ('Randomized Stored Data', 0)
+        self._modeSelect += ('Client Connect Windows', 1)
+        self._modeSelect += ('Client Connect MacOS', 2)
+
+        self._epochDisplay.enabled = False
+
         self.formset = [{
             'a:Options': ['_runName', '_readyButton'],
-            'b:Predictions': [''],
-            'c:BoardState': ['_boardStateTree', '_updateBoardState']
-        },
-            '=', ('_runButton', '=', '_runOnceButton'), '||', '_modeSelect']
+            'b:Predictions': [(), '||', ('_cardToPlayImage', '_cardToPlayGL', '_optionToTakeDescription')],
+            'c:BoardState': ['_boardStateTree', '_updateBoardStateButton']},
+            '=',
+            ('_runButton', '=', '_runOnceButton'), '||', '_modeSelect'
+        ]
         # Use dictionaries for tabs
         # Use the sign '=' for a vertical splitter
         # Use the signs '||' for a horizontal splitter
@@ -56,7 +70,7 @@ class HearthGUI(BaseWidget):
     def _exit(self):
         exit(0)
 
-    def __ready(self):
+    def _ready(self):
         """ Check that the runName doesn't already exist """
         if (os.path.isdir("./logs/" + self._runName.value)):
             #print error
@@ -69,11 +83,11 @@ class HearthGUI(BaseWidget):
         network.ready()
         # Run a first loop
 
-    def __runNetwork(self):
+    def _runNetwork(self):
         """Button action event"""
         self._runButton.label = "Running"
 
-    def __runNetworkOnce(self):
+    def _runNetworkOnce(self):
         """Button action event"""
         self._runOnceButton.label = "Running"
         self._runOnceButton.enabled = False
@@ -82,6 +96,8 @@ class HearthGUI(BaseWidget):
         mode = self._modeSelect
         print(mode)
 
+    def _updateBoardState(self):
+        """Button action event"""
 
 #Execute the application
 if __name__ == "__main__":   pyforms.start_app(HearthGUI)
